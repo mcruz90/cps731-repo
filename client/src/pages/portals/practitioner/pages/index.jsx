76aa449523d57dae0cc43ddd489851d7874f0b52
Stewart Bustard <stewart.bustard@gmail.com>
2024-11-28 17:31:21 -0500

    Removed book appointment function

--- client/src/pages/portals/practitioner/pages/index.jsx
+++ client/src/pages/portals/practitioner/pages/index.jsx
@@ -4,14 +4,12 @@ import PortalNav from '@/components/Layout/PortalNav';
 import Dashboard from './Dashboard';
 import Schedule from './Schedule';
 import Clients from './Clients';
-import BookAppointment from './BookAppointment';
 
 const PractitionerPortal = () => {
   const navLinks = [
     { to: '/practitioner', label: 'Dashboard' },
     { to: '/practitioner/schedule', label: 'Schedule' },
     { to: '/practitioner/clients', label: 'Clients' },
-    { to: '/practitioner/book', label: 'Book Appointment' },
   ];
 
   return (
@@ -21,7 +19,6 @@ const PractitionerPortal = () => {
         <Route index element={<Dashboard />} />
         <Route path="schedule" element={<Schedule />} />
         <Route path="clients" element={<Clients />} />
-        <Route path="book" element={<BookAppointment />} />
       </Routes>
     </>
   );
